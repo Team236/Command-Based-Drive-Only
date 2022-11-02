bc7b06de0c95c38a6e480848b059f94271d5842a
matherm632 <matherm@region18.org>
2022-11-01 20:17:01 -0400

    initial

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+      // Stores joystick port numbers
+      public static final int USB_LEFT_STICK = 0;
+      public static final int USB_RIGHT_STICK = 1;
+      public static final int USB_CONTROLLER = 2;
+  
+      // Stores motor controller ID numbers
+      public static final int ID_LEFT_FRONT = 30;
+      public static final int ID_LEFT_REAR = 44;
+      public static final int ID_RIGHT_FRONT = 43;
+      public static final int ID_RIGHT_REAR = 45;
+}
